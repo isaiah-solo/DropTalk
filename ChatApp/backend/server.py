a314fd7096fd92ca31f5b9971afdd88c604ce49a
Josef Bustamante <kfbustam@ucsc.edu>
2017-01-21 03:22:22 -0800

    inserting into datastore db

--- ChatApp/backend/server.py
+++ ChatApp/backend/server.py
@@ -1,7 +1,11 @@
 import logging
 
+# Imports the Google Cloud client library
+from google.cloud import datastore
 from flask import Flask
 app = Flask(__name__)
+
+counter=0
 
 @app.route("/get", methods = ["GET"])
 def get():
@@ -9,7 +13,27 @@ def get():
 
 @app.route("/post", methods = ["POST"])
 def post():
-    return "Hello World"
+	message=''
+	# Instantiates a client
+	datastore_client = datastore.Client()
+
+	# The kind for the new entity
+	kind = 'Message'
+	# The name/ID for the new entity
+	name = str(counter++)
+	# The Cloud Datastore key for the new entity
+	task_key = datastore_client.key(kind, name)
+
+	# Prepares the new entity
+	task = datastore.Entity(key=task_key)
+	task['description'] = message
+
+	# Saves the entity
+	datastore_client.put(task)
+
+	print('Saved {}: {}'.format(task.key.name, task['description']))
+	return "Hello World"
+
 
 @app.errorhandler(500)
 def server_error(e):
